a735fd1465c3fc6725f1eab968158e52611de4a7
Natalia Gordeeva <ganya85@gmail.com>
2022-08-22 23:45:40 +0300

    задача1

---
+++ 1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1= ");
+Console.WriteLine("Введите число 2= ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+     Console.WriteLine("Число 1 больше числа 2");
+}
+else
+{
+     Console.WriteLine("Число 2 больше числа 1");
+ }
