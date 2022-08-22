b489af3d17364a200f398c0375291628cc6a360e
Natalia Gordeeva <ganya85@gmail.com>
2022-08-23 00:15:58 +0300

    задача2

---
+++ 1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число 1= ");
+Console.WriteLine("Введите число 2= ");
+Console.WriteLine("Введите число 3= ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+      Console.WriteLine("Число 1 - max");   
+    }
+    else
+     Console.WriteLine("Число 3 - max");
+}
+else
+{
+ if (number2 > number3)
+ Console.WriteLine("Число 2 - max");
+ else
+ {
+    Console.WriteLine("Число 3 - max");
+ }
+}
