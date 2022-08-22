cb97b08be16eaf4f4e2fc28eea6e0e9f556cb984
Natalia Gordeeva <ganya85@gmail.com>
2022-08-23 00:18:26 +0300

    задача1

--- 1_1/Program.cs
+++ 1_1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число 1= ");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число 1= ");
 Console.WriteLine("Введите число 2= ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
