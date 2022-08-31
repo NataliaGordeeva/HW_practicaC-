815775b8a9895a77312cb5a4fdbf4dced1138280
Natalia Gordeeva <ganya85@gmail.com>
2022-08-31 14:13:16 +0300

    3_1 задача

---
+++ 3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. 23432 -> да
+
+string num = Console.ReadLine();
+
+for (int i = 0; i < (num.Length - 1) / 2; i++)
+
+if (num[i] != num[num.Length - i - 1])
+Console.WriteLine(false);
+
+else
+Console.WriteLine(true);
+
