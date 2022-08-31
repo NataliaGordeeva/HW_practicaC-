a0799db7079665d8ea74c70c5ef5ee3e93f0a3ba
Natalia Gordeeva <ganya85@gmail.com>
2022-08-31 13:41:42 +0300

    3_3 hw

--- 3_3/Program.cs
+++ 3_3/Program.cs
@@ -1,8 +1,10 @@
-﻿Console.WriteLine("введите число N");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("введите число N");
 int N = Convert.ToInt32(Console.ReadLine());
 int a = 1;
 while (a< N)
 {
-    Console.Write(Math.Pow(a,2)+" ");
+    Console.Write(Math.Pow(a,3)+" ");
     a++;
 } 
