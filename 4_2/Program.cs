ee6a46e47002a9a06249f935c0cb6095cf9ec6ba
Natalia Gordeeva <ganya85@gmail.com>
2022-09-03 14:50:55 +0300

    4_3 задача

---
+++ 4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе (452 -> 11, 
+// 82 -> 10, 9012 -> 12)
+
+Console.WriteLine("введите число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("сумма цифр = "+ GetSummary(num));
+
+int GetSummary (int number)
+{
+   int sum = 0;
+   {
+   for (int i = 0; i < Ch.Count; i++)
+   sum = sum + i;
+   }
+
+  return sum;
+}
+
---
+++ 4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+int[] array = new int[8];
+
+void GetMassiv(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index<lenght)
+    {
+        collection[index]=new Random().Next(0, 99);
+        index++;
+    }
+}
+
+GetMassiv(array);
+Console.WriteLine("массив:{0} ",String.Join(" ",array));
