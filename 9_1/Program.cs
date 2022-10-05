6e058302f39d926c2bf9ced2f7ad9872d94d96d8
Natalia Gordeeva <ganya85@gmail.com>
2022-10-05 15:32:52 +0300

    9_1

---
+++ 9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Программа выводит все натуральные числа в промежутке от 1 до N: N = 5 -> "1, 2, 3, 4, 5".
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+if (num<start) Console.WriteLine("Введено неверное число");
+
+string NumRange (int numStart,int numEnd)
+{
+     if(numStart<=numEnd) 
+    return $"{numStart} "+ NumRange(numStart+1,numEnd);
+    else return string.Empty;
+}
+Console.WriteLine(NumRange(start,num));
